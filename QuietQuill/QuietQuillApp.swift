c2cef52c8eeb7bdc4f1ffe3073103a16979a2ba6
YashavikaSingh <65505787+YashavikaSingh@users.noreply.github.com>
2025-01-16 21:35:46 +0530

    Initial Commit

---
+++ QuietQuill/QuietQuillApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietQuillApp.swift
+//  QuietQuill
+//
+//  Created by Yashavika Singh on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct QuietQuillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuietQuillTests/QuietQuillTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuietQuillTests.swift
+//  QuietQuillTests
+//
+//  Created by Yashavika Singh on 16.01.25.
+//
+
+import Testing
+@testable import QuietQuill
+
+struct QuietQuillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
